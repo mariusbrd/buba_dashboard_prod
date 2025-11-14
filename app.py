4356332f8b9446ab17b4a0c28b0fb3a6fac91fa5
mariusbrd <mariusbrede@gmail.com>
2025-11-14 16:02:29 +0100

    Disable debug mode for production

--- app.py
+++ app.py
@@ -4235,6 +4235,6 @@ if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=8080,
-        debug=True
-        # debug=False
+        # debug=True
+        debug=False
     )
